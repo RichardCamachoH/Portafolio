e9c3ae8c5d8e4132268e71b9d4654e1db97c162a
Richard Camacho <richard.camacho@nivelics.co>
2021-12-07 18:10:46 -0500

    continuacion ejercicio sass

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".nav-menu");
+const navToggle = document.querySelector(".header__toggle");
+const navMenu = document.querySelector(".header__list");
 
 navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("nav-menu_visible")
-    navMenu.classList.toggle("nav-menu_no_visible")
+    navMenu.classList.toggle("menu-visible")
+    navMenu.classList.toggle("menu-hidden")
 });
 
-const menuOpc = document.querySelectorAll('.nav-menu a[href^="#"]')
+const menuOpc = document.querySelectorAll('.header__list a[href^="#"]')
 
 menuOpc.forEach(element => {
     element.addEventListener("click", function() {
-        navMenu.classList.toggle("nav-menu_visible")
-        navMenu.classList.toggle("nav-menu_no_visible")
+        navMenu.classList.toggle("menu-visible")
+        navMenu.classList.toggle("menu-hidden")
     })
 });
